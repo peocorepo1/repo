9339b5bb3df2970347b011ac43994312dd3f8700
PEOCO5 <36616218+PEOCO5@users.noreply.github.com>
2018-11-29 16:58:27 -0500

    init commit

--- EQE/data/var/tweak/com.r333d.eqe/lua/cli/util.lua
+++
@@ -1,5 +0,0 @@
-
-
-function math.round(x)
-    return math.floor(x + 0.5)
-end
--- EQE/data/var/tweak/com.r333d.eqe/lua/core/object.lua
+++
@@ -1,7 +0,0 @@
-local object = {}
-
-function object:new()
-    return setmetatable({}, {__index = self})
-end
-
-return object
